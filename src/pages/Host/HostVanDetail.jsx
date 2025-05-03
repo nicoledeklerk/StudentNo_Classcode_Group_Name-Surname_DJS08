9db2a6acab9be510edba2175fb9428fed6ed5012
Nicole de Klerk <nicolenataliedeklerk@gmail.com>
2025-05-03 12:49:37 +0200

    Add & Style Final Navigation Bar

--- src/pages/Host/HostVanDetail.jsx
+++ src/pages/Host/HostVanDetail.jsx
@@ -1,10 +1,16 @@
 import React from "react"
-import { useParams, Link, Outlet } from "react-router-dom"
+import { useParams, Link, NavLink, Outlet } from "react-router-dom"
 
 export default function HostVanDetail() {
 
     const { id } = useParams()
     const [currentVan, setCurrentVan] = React.useState(null)
+
+    const activeStyles = {
+        fontWeight: "bold",
+        textDecoration: "underline",
+        color: "#161616"
+    }
 
     React.useEffect(() => {
         fetch(`/api/host/vans/${id}`)
@@ -37,6 +43,32 @@ export default function HostVanDetail() {
                     <h4>${currentVan.price}/day</h4>
                 </div>
             </div>
+
+            <nav className="host-van-detail-nav">
+                    <NavLink
+                        to="."
+                        style={({ isActive }) => isActive ? activeStyles : null}
+                        end
+                    >
+                        Details
+                    </NavLink>
+                    
+                    <NavLink
+                        to="pricing"
+                        style={({ isActive }) => isActive ? activeStyles : null}
+                    >
+                        Pricing
+                    </NavLink>
+
+                    <NavLink
+                        to="photos"
+                        style={({ isActive }) => isActive ? activeStyles : null}
+                    >
+                        Photos
+                    </NavLink>
+
+            </nav>
+
             <Outlet />
         </div>
     </section>
