05e3da7ef5592101b6fe4aa924a8249fcaf1d089
Nicole de Klerk <nicolenataliedeklerk@gmail.com>
2025-05-03 20:36:22 +0200

    Create & Implement 404 Not Found Component

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import HostVanDetail from "./pages/Host/HostVanDetail"
 import HostVanInfo from "./pages/Host/HostVanInfo"
 import HostVanPricing from "./pages/Host/HostVanPricing"
 import HostVanPhotos from "./pages/Host/HostVanPhotos"
+import NotFound from "./pages/NotFound"
 import Layout from "./Components/Layout" 
 import HostLayout from "./Components/HostLayout"
 
@@ -39,6 +40,7 @@ function App() {
               <Route path="photos" element={<HostVanPhotos />} />
             </Route>
           </Route>
+          <Route path="*" element={<NotFound />}/>} />
         </Route>
       </Routes>
     </BrowserRouter>
