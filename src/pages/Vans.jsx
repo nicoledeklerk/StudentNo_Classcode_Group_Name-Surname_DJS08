6861192f34f1d27eb009be84b5c40be2b4d7bb4f
Nicole de Klerk <nicolenataliedeklerk@gmail.com>
2025-05-03 14:08:33 +0200

    Filter Vans Using Search Params

--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -1,27 +1,23 @@
 import React from "react"
-import { Link } from "react-router-dom"
-
-/**
- * {
-    * id: "1", 
-    * name: "Modest Explorer", 
-    * price: 60, 
-    * description: "The Modest Explorer is a van designed to get you out of the house and into nature. This beauty is equipped with solar panels, a composting toilet, a water tank and kitchenette. The idea is that you can pack up your home and escape for a weekend or even longer!", 
-    * imageUrl: "https://assets.scrimba.com/advanced-react/react-router/modest-explorer.png", 
-    * type: "simple"
- * }
- */
-
+import { Link, useSearchParams } from "react-router-dom"
 
 export default function Vans() {
+    const [searchParams, setSearchParams] = useSearchParams()
     const [vans, setVans] = React.useState([])
+    
+    const typeFilter = searchParams.get("type")
+    
     React.useEffect(() => {
         fetch("/api/vans")
             .then(res => res.json())
             .then(data => setVans(data.vans))
     }, [])
+    
+    const displayedVans = typeFilter
+        ? vans.filter(van => van.type === typeFilter)
+        : vans
 
-    const vanElements = vans.map(van => (
+    const vanElements = displayedVans.map(van => (
         <div key={van.id} className="van-tile">
             <Link to={`/vans/${van.id}`}>
                 <img src={van.imageUrl} />
@@ -36,6 +32,7 @@ export default function Vans() {
 
     return (
         <div className="van-list-container">
+            <h1>Explore our van options</h1>
             <div className="van-list">
                 {vanElements}
             </div>
